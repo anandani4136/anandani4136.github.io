ae27ec042b07016945c237eeab17fcdcf5e42786
anandani4136 <ra7353@hotmail.com>
2024-01-10 01:09:43 -0600

    Site Revamp
    Added About, Experience, and Projects sections, with transition animations and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import 'animate.css'
+import Navbar from './Navbar/navbar'
 import Title from './title'
+import Transition from './Transition/transition'
+import About from './About/about'
 import Experience from './Experience/experience'
+import Projects from './Projects/projects'
+import Footer from './Footer/footer'
 
 export default function Home() {
+  
   return (
     <div>
-      {/* <Title/> */}
+      <Navbar/>
+      <Title/>
+      <Transition/>
+      <About/>
       <Experience/>
+      <Projects/>
+      <Footer/>
     </div>
     
   )
--- src/app/title.tsx
+++ src/app/title.tsx
@@ -1,14 +1,133 @@
+"use client";
+
 import Image from 'next/image'
 import styles from './page.module.css'
 import 'animate.css'
+import { useEffect, useState, useRef } from 'react';
 
 export default function Title() {
+  const [textOpacity, setTextOpacity] = useState(0);
+  const [textOpacity2, setTextOpacity2] = useState(0);
+  const [textOpacity3, setTextOpacity3] = useState(0);
+  const [opacity, setOpacity] = useState(1);
+  const [opacity2, setOpacity2] = useState(1);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(20);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPercent, setScrollPercent] = useState(0);
+  const [maxWidth, setMaxWidth] = useState(0.5);
+  const textRef = useRef(null);
+  const textRef2 = useRef(null);
+  const textRef3 = useRef(null);
+
+  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.pageYOffset;
+      const windowHeight = window.innerHeight;
+      const middleScreen = windowHeight / 2;
+      setScrollPosition(position);
+      setOpacity(1 - position / 500);
+
+      const scrollY = window.scrollY; // Get the current scroll position
+      const totalScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+      // get scroll percentage
+      setScrollPercent(scrollY / window.innerHeight);
+      // console.log(scrollPercent);
+      const newWidth = Math.min(100, (scrollY / totalScrollHeight) * 970); // Calculate new width as a percentage
+      setWidth(Math.min(window.innerWidth, newWidth));
+
+      if (textRef.current) {
+        const divRect = (textRef.current as HTMLElement).getBoundingClientRect();
+        const divTop = divRect.top;
+        const divBottom = divRect.bottom;
+        if (divTop < 1.5*middleScreen && divBottom > 0.5*middleScreen) {
+          // console.log(divTop + " " + divBottom);
+          // Calculate opacity based on distance to the middle of the screen
+          const distanceToMiddle = Math.abs(middleScreen - (divTop + divRect.height / 2));
+          const newOpacity = 1 - (6 * distanceToMiddle / windowHeight);
+          setTextOpacity(Math.max(0, newOpacity));
+        } else {
+          setTextOpacity(0);
+        }
+      }
+
+      if (textRef2.current) {
+        const divRect = (textRef2.current as HTMLElement).getBoundingClientRect();
+        const divTop = divRect.top;
+        const divBottom = divRect.bottom;
+        if (divTop < 1.5*middleScreen && divBottom > 0.5*middleScreen) {
+          // console.log(divTop + " " + divBottom);
+          // Calculate opacity based on distance to the middle of the screen
+          const distanceToMiddle = Math.abs(middleScreen - (divTop + divRect.height / 2));
+          const newOpacity = 1 - (6 * distanceToMiddle / windowHeight);
+          setTextOpacity2(Math.max(0, newOpacity));
+        } else {
+          setTextOpacity3(0);
+        }
+      }
+
+      if (textRef3.current) {
+        const divRect = (textRef3.current as HTMLElement).getBoundingClientRect();
+        const divTop = divRect.top;
+        const divBottom = divRect.bottom;
+        if (divTop < 1.5*middleScreen && divBottom > 0.5*middleScreen) {
+          // console.log(divTop + " " + divBottom);
+          // Calculate opacity based on distance to the middle of the screen
+          const distanceToMiddle = Math.abs(middleScreen - (divTop + divRect.height / 2));
+          const newOpacity = 1 - (6 * distanceToMiddle / windowHeight);
+          setTextOpacity3(Math.max(0, newOpacity));
+        } else {
+          setTextOpacity3(0);
+        }
+      }
+
+      if (width === 100) {
+        // setMaxWidth(Math.min(maxWidth, ));
+        // console.log((scrollY / totalScrollHeight))
+        // Calculate scroll offset from the point where width reached 100%
+        const widthReachedFullAt = totalScrollHeight * 0.038;
+        const excessScroll = scrollY - widthReachedFullAt;
+
+        if (excessScroll > 0) {
+          // Start increasing height only after width has reached 100%
+          // const newHeight = Math.min(window.innerHeight, (excessScroll / (totalScrollHeight - widthReachedFullAt)) * window.innerHeight);
+          // setHeight(Math.min(window.innerHeight, newHeight));
+          const newHeight = Math.min((excessScroll - 0.046*totalScrollHeight) / (totalScrollHeight) * 970);
+          setHeight(Math.min(newHeight, 100));
+          // console.log((newHeight - 100)/100);
+          setOpacity2(1 - 1.2*(newHeight - 100)/100);
+        } else {
+          setHeight(20);
+        }
+      } else {
+        setHeight(20); // Reset height if width is less than window's width
+      }
+    };
+    
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    // requestAnimationFrame(handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }
+  , [scrollPercent, width]);
+
+  onload = () => {
+    const element = document.getElementById('navbar');
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+
     return (
-        <main className={styles.main}>
+        <main id='header' className={styles.main} style={{backgroundColor: `${scrollPercent < 0.15 ? 'black' : ''}`, }}>
           <div className={styles.description}>
-            <div className={styles.textContainer}>
+            {/* <div className={styles.textContainer}>
               <h2 className={styles.typeTag}>Greetings, my name is</h2>
-            </div>
+            </div> */}
             
             {/* <p>
               Get started by editing&nbsp;
@@ -32,9 +151,12 @@ export default function Title() {
               </a>
             </div> */}
           </div>
-    
-          <div className={styles.center}>
-            <h1 className='animate__animated animate__backInDown animate__delay-2s'>Ronit Anandani</h1>
+          
+          <div className={styles.center} style={{ opacity: opacity }}>
+            <div className={styles.name}> 
+              <h1 className={`animate__animated animate__slideInUp ${styles.fname}`}>Ronit </h1>
+              <h1 className={`animate__animated animate__slideInDown ${styles.lname}`}> Anandani</h1>
+            </div>
             {/* <Image
               className={styles.logo}
               src="/next.svg"
@@ -44,9 +166,25 @@ export default function Title() {
               priority
             /> */}
           </div>
+          <div className={styles.fadeBox} style={{opacity: opacity2}}>
+            {/* {scrollPercent < 0.445 ? ( */}
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: `${width}%`, height: `${height}%`, backgroundColor: '#04217C', position: 'fixed', zIndex: -1, transform: `translate3d(0, 0, 0)`,
+            filter: `blur(${(height - 100) > 0 ? 45 : 45}px)` 
+            }}> 
+              <div style={{ width: `90%`, height: `${height-10}%`, backgroundColor: '#000000', position: 'fixed', zIndex: -1, 
+            filter: `blur(45px)` 
+            }}/> 
+
+            </div>
+          {/* ) : ( */}
+            {/* <div style={{ width: `${width}%`, height: '200px', backgroundColor: '#04217C', position: 'absolute', bottom: '0%' }}/> */}
+          {/* )} */}
+          </div>
+
+          
     
           <div className={styles.grid}>
-            <a
+            {/* <a
               href="https://github.com/anandani4136"
               className={`animate__animated animate__backInUp animate__delay-3s ${styles.card}`}
               target="_blank"
@@ -102,7 +240,17 @@ export default function Title() {
               <p>
                 View the code of this website, soon to be updated!
               </p>
-            </a>
+            </a> */}
+            
+          </div>
+          <div ref={textRef3} className={styles.text3} style={{opacity: textOpacity3}}>
+              <h2>I'm a problem-solver</h2>
+          </div>
+          {/* <div ref={textRef2} className={styles.text2} style={{opacity: textOpacity2}}>
+              <h2>I'm a problem-solver</h2>
+          </div> */}
+          <div ref={textRef} className={styles.text1} style={{opacity: textOpacity}}>
+              <h2>who loves to continuously ideate</h2>
           </div>
         </main>
       )
