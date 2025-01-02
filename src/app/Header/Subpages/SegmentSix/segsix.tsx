7ad488712033614d8cc4ac74aa76a14f3f86c65b
anandani4136 <ra7353@hotmail.com>
2025-01-02 01:58:49 -0600

    Segment Six + Wave Component

---
+++ src/app/Header/Subpages/SegmentSix/segsix.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styles from "./segsix.module.css";
+
+export default function SegmentSix() {
+    return (
+        <div className={styles.segmentSix}>
+            <h1 className={styles.centerText}>
+                This is Segment Six
+            </h1>
+        </div>
+    );
+}
