b6d8b93f975ee0e0504cc3fe5fa5b1dff387d10e
anandani4136 <ra7353@hotmail.com>
2025-01-02 01:54:25 -0600

    Update About Page

--- src/app/About/about.tsx
+++ src/app/About/about.tsx
@@ -33,11 +33,11 @@ export default function About() {
             }
         }
 
-        AOS.init({
-            easing: "ease-out-cubic",
-            once: true,
-            offset: 50,
-          });
+        // AOS.init({
+        //     easing: "ease-out-cubic",
+        //     once: true,
+        //     offset: 50,
+        //   });
 
         window.addEventListener('scroll', handleScroll);
         return () => {
@@ -46,27 +46,27 @@ export default function About() {
     }, []);
 
     const imageStyle = {
-        transform: `translateY(${scrollPosition * 0.07*divHeight - 0.35*divHeight}px) translate3d(0, 0, 0)`, // Adjust this value to control the speed of scrolling
+        transform: `translateY(${scrollPosition * 0.06*divHeight - 0.5*divHeight}px) translate3d(0, 0, 0)`, // Adjust this value to control the speed of scrolling
     };
 
     return (
-        <div ref={divRef} id="about-main" className={styles.main}>
+        <div ref={divRef} id="about" className={styles.main}>
             <div className={styles.image} ref={ref} style={ imageStyle
                 //inView ? imageStyle : undefined
                 }>
-                <img className={styles.img} src='/media/ronit.PNG' />
+                <img className={styles.img} src='/media/ronit.PNG' alt="My Photo" />
             </div>
             <div className={styles.description}>
                 <div className={styles.about}>
-                    <h2 data-aos="fade-up" data-aos-duration="700">Greetings!</h2>
-                    {/* <br></br> */}
+                    <h1 data-aos="fade-up" data-aos-duration="700">Greetings! 👋</h1>
+                    <br></br>
                     <h2 data-aos="fade-up" data-aos-duration="1000">
                         My name is <b>Ronit Anandani</b>. I am a <b>Computer Science Student</b> at the University of Illinois
                         Urbana-Champaign.
                     </h2>
                     {/* <br></br> */}
                     <h2 data-aos="fade-up" data-aos-duration="1300">
-                        I&apos;m a <u>self-driven engineer</u> committed to creating <b>Robust</b>, <b>Efficient</b>, and <b>Innovative</b> products. 
+                        I&apos;m a <u>self-driven engineer</u> committed to creating <b>Impactful</b> and <b>Innovative</b> products. 
                         I believe in augmenting my learning by taking on novel challenges in different domains, 
                         and I&apos;m continuously pursuing new ways to advance the projects, teams, and knowledge that I engage with.
                     </h2>
